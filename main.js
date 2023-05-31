0cf5df7fce83f88ba4906085ae019372c969423a
Sarhan Alim <sarhan.alim@gmail.com>
2023-05-31 16:53:34 +0100

    add reset & clear counter btn functionality

--- main.js
+++ main.js
@@ -6,13 +6,15 @@ let saveBtn = document.getElementById('save-btn');
 let decreaseBtn = document.getElementById('decrease-btn');
 let savedEl = document.getElementById('saved-el');
 let totalEl = document.getElementById('total-el');
+let resetBtn = document.getElementById('reset-btn');
+let clearCounterBtn = document.getElementById('clear-counter-btn');
 
 let count = 0;
 let total = 0;
 
 incrementBtn.addEventListener('click', () => {
   const increment = function () {
-    count ++;
+    count++;
     countEl.textContent = count;
   };
   increment();
@@ -20,7 +22,7 @@ incrementBtn.addEventListener('click', () => {
 
 saveBtn.addEventListener('click', () => {
   const save = function () {
-    let countStr = `${count} - `;
+    let countStr = `${count} ◽ `;
     savedEl.textContent += countStr;
     total += count;
     totalEl.textContent = total;
@@ -34,6 +36,18 @@ decreaseBtn.addEventListener('click', () => {
   const decrease = function () {
     count--;
     countEl.textContent = count;
-  }
+  };
   decrease();
 });
+
+resetBtn.addEventListener('click', () => {
+  confirm('Are you sure? Entries & total will be lost!')
+    ? location.reload()
+    : '';
+});
+
+clearCounterBtn.addEventListener('click', () => {
+    countEl.textContent = 0;
+    count = 0;
+});
+
