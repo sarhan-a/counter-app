2a6092ae1c4b1e5b49ca0e924e9584afa5cdb654
Sarhan Alim <sarhan.alim@gmail.com>
2023-05-31 11:52:00 +0100

    add decrease btn & calculate total, style changes

--- main.js
+++ main.js
@@ -3,8 +3,12 @@
 let countEl = document.getElementById('count-el');
 let incrementBtn = document.getElementById('increment-btn');
 let saveBtn = document.getElementById('save-btn');
+let decreaseBtn = document.getElementById('decrease-btn');
 let savedEl = document.getElementById('saved-el');
+let totalEl = document.getElementById('total-el');
+
 let count = 0;
+let total = 0;
 
 incrementBtn.addEventListener('click', () => {
   const increment = function () {
@@ -18,8 +22,18 @@ saveBtn.addEventListener('click', () => {
   const save = function () {
     let countStr = `${count} - `;
     savedEl.textContent += countStr;
+    total += count;
+    totalEl.textContent = total;
     countEl.textContent = 0;
     count = 0;
   };
   save();
+});
+
+decreaseBtn.addEventListener('click', () => {
+  const decrease = function () {
+    count--;
+    countEl.textContent = count;
+  }
+  decrease();
 });
